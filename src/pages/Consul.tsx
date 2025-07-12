d9a2e950ca853273ce612a2fbb0663590021d2cc
Ahmed Magdy <104032428+AhmedMagdy199@users.noreply.github.com>
2025-07-12 22:40:14 +0300

    Add basic Consul component structure

---
+++ src/pages/Consul.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Consul: React.FC = () => {
+  return (
+    <div>
+      <h1>Consul</h1>
+      <p>Consul page content will be implemented here.</p>
+    </div>
+  );
+};
+
+export default Consul;
